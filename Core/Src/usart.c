c704a86fd48230fd741c874e4e87b5591bcf6645
CYKEFEW <3622801243@qq.com>
2026-04-09 20:26:10 +0800

    fixbug

--- Core/Src/usart.c
+++ Core/Src/usart.c
@@ -85,9 +85,9 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
                     printf("Set Control Mode: Speed\n");
                 }
             }
-            else if(strncmp((char *)usart1_cmd_buf, "FOCCM", 5) == 0){
+            else if(strncmp((char *)usart1_cmd_buf, "focCM", 5) == 0){
                 char foc_command[32];
-                sscanf((char *)usart1_cmd_buf, "FOCCM %s",foc_command);
+                sscanf((char *)usart1_cmd_buf, "focCM %s",foc_command);
                 focCommand(foc_command);
                 printf("Send FOC command: %s\n",foc_command);
             }
